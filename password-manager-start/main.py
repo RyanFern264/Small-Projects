1b7afc3a20f7c221cc667246fc806f4a5b6d73ff
rfern264 <ryanfmiami@gmail.com>
2024-12-22 15:55:49 -0500

    Updated password-manager
    changed file storage format to json, added error handling, created search button to search existing website credentials

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
+from param.ipython import message
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -30,24 +34,51 @@ def generate_password():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
-    website_entry.delete(0, END)
-
     email = email_entry.get()
-
     password = password_entry.get()
-    password_entry.delete(0, END)
+
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if website and password:
-        full_creds = website + "," + email + "," + password + "\n"
+        try:
+            with open("cred_storage.json", mode="r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("cred_storage.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
 
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
-
-        if is_ok:
-            with open("cred_storage.csv", mode="a") as cred_storage:
-                cred_storage.write(full_creds)
+            with open("cred_storage.json", mode="w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
     else:
         messagebox.showinfo(title="One or more fields emtpy", message="Please don't leave any fields empty silly!")
+# ---------------------------- CRED RETRIEVAL ------------------------------- #
+def search_creds():
+    website = website_entry.get()
+    try:
+        with open("cred_storage.json", mode="r") as cred_storage:
+            # Reading old data
+            data = json.load(cred_storage)
+    except FileNotFoundError:
+        print("You don't have anything saved")
+    else:
+        if website in data:
+            messagebox.showinfo(title=f"{website}", message=f"Email: {data[website]['email']}\n"
+                                                            f"Password: {data[website]['password']}")
+        else:
+            messagebox.showinfo(title="No website info", message=f"No details for {website} exist")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -70,7 +101,7 @@ password_label = Label(text="Password: ")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=32)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="w")
 website_entry.focus()
 
@@ -87,5 +118,8 @@ password_gen_button.grid(row=3, column=2, sticky="w")
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="w")
+
+search_button = Button(text="Search", command=search_creds)
+search_button.grid(row=1, column=2, sticky="w")
 
 window.mainloop()
